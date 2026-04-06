270ed4959b5d3b7a138767fefef3c26a07e451f0
taras-gr <tar.grytsenko@gmail.com>
2026-04-06 13:15:03 +0300

    Added MediatR license

--- TarikBooks.Api/Program.cs
+++ TarikBooks.Api/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddControllers();
 
 builder.Services.AddDalServices(builder.Configuration);
 
-builder.Services.AddApplicationServices();
+builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
--- TarikBooks.Application/ApplicationServicesRegistration.cs
+++ TarikBooks.Application/ApplicationServicesRegistration.cs
@@ -1,12 +1,17 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 
 namespace TarikBooks.Application;
 
 public static class ApplicationServicesRegistration
 {
-    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(ApplicationServicesRegistration).Assembly));
+        services.AddMediatR(cfg => {
+            cfg.LicenseKey = configuration["LicenseKeys:MediatRLicenseKey"];
+            cfg.RegisterServicesFromAssembly(typeof(ApplicationServicesRegistration).Assembly);
+        });
+
         return services;
     }
 }
